ffe27ebc985227e2edb5cece7d1259d086c259bd
sumeyrademirtas <smyrdmrts@gmail.com>
2024-05-30 17:07:26 +0300

    #deneme

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class DetailsVC: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var artistText: UITextField!
+    
+    @IBOutlet weak var yearText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +24,9 @@ class DetailsVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        print("Save button clicked")
     }
-    */
+   
 
 }
