34d77308ca01eaa8c70b5b15457c31017f679fe2
sumeyrademirtas <smyrdmrts@gmail.com>
2024-05-31 12:05:44 +0300

    #feat(ui): add image to assets #feat(ui): add UITapGestureRecognizer #feat(ui): add UIImagePickerController

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -22,8 +22,17 @@ class DetailsVC: UIViewController {
 
         // Do any additional setup after loading the view.
         
+        
+        //Recognizers
+        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+        
+        
     }
     
 
@@ -33,6 +42,25 @@ class DetailsVC: UIViewController {
     
     @objc func hideKeyboard () {
         view.endEditing(true)
+    }
+    
+    @objc func selectImage () {
+        
+        let picker = UIImagePickerController() // picker fotograflara erismek icin
+        picker.delegate = self // bunun da fonksiyonlarini cagirabilmek icin delegate olarak tanimladik. yukarida ekledik.
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    
+    //media ile isimiz bitince, secince, bu fonksiyon bana dictionary donduruyor. any type gorselin type i. biz tipini bilmedigimiz icin basta any olarak geliyor.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage //genelde original ya da editedImage kullanilir
+        self.dismiss(animated: true, completion: nil) //pickeri geri kapatmak icin
+        
     }
    
 
